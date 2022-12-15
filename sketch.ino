ce32dcd2a9892c88ae9fa3c64f99ecc04459626b
Mena Rizkalla <52071472+mena-rizkalla@users.noreply.github.com>
2022-12-15 16:36:51 +0000

    add comments

--- sketch.ino
+++ sketch.ino
@@ -1,13 +1,16 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd = {1, 2, 3, 4, 5, 6}; // can be modified
 
+//sensors
 int irLeft = A0;
 int irRight = A1;
 int irFront = A2;
 int irBehind = A3;
-int leftMotorPin1 = 4;
+//left motor
+int leftMotorPin1 = 4; 
 int leftMotorPin2 = 7;
 int enLeft = 6;
+//right motor
 int rightMotorPin1 = 2;
 int rightMotorPin2 = 3;
 int enRight = 5;
@@ -21,8 +24,10 @@ void setup()
 {
   lcd.begin(16, 2);
   lcd.setCursor(3, 1);
+  // Print a message to the LCD.
   lcd.println("START");
   
+  // initialize the inputs and outputs
   pinMode(enLeft, OUTPUT);
   pinMode(enRight, OUTPUT);
   pinMode(leftMotorPin1, OUTPUT);
@@ -96,36 +101,43 @@ void loop()
 
 boolean sensingNothing()
 {
+  //stop when both sensors read 0
   return digitalRead(irFront) == ground && digitalRead(irBehind) == ground;
 }
 
 boolean sensingLeft()
 {
+  //move left when left sensor read 1 
   return digitalRead(irRight) == ground && digitalRead(irLeft) == line;
 }
 
 boolean sensingRight()
 {
+  //move right when right sensor read 1
   return digitalRead(irRight) == line && digitalRead(irLeft) == ground;
 }
 
 boolean sensingStraight()
 {
+  // move forward when both sensors read 1
   return digitalRead(irFront) == line || digitalRead(irBehind) == line ;
 }
 
 boolean cross()
 {
+  //return true when all sesnors read 1 (a cross found)
   return digitalRead(irRight) == line && digitalRead(irLeft) == line && digitalRead(irFront) == line;
 }
 
 boolean gap()
 {
+  //return ture when all sensors read 0 (a gap found)
   return digitalRead(irRight) == ground && digitalRead(irLeft) == ground && digitalRead(irFront) == ground;
 }
 
 void goRight()
 {
+  //Tilt robot towards right by stopping the right wheel and moving the left one
   digitalWrite(rightMotorPin1, LOW);
   digitalWrite(rightMotorPin2, HIGH);
   digitalWrite(leftMotorPin1, HIGH);
@@ -136,6 +148,7 @@ void goRight()
 }
 void goLeft()
 {
+  //Tilt robot towards left by stopping the left wheel and moving the right one
   digitalWrite(rightMotorPin1, HIGH);
   digitalWrite(rightMotorPin2, LOW);
   digitalWrite(leftMotorPin1, LOW);
@@ -146,6 +159,7 @@ void goLeft()
 }
 void goForward()
 {
+  //Move both the Motors
   digitalWrite(rightMotorPin1, HIGH);
   digitalWrite(rightMotorPin2, LOW);
   digitalWrite(leftMotorPin1, HIGH);
@@ -155,6 +169,7 @@ void goForward()
 }
 void stop()
 {
+  //Stop both the motors
   digitalWrite(rightMotorPin1, LOW);
   digitalWrite(rightMotorPin2, LOW);
   digitalWrite(leftMotorPin1, LOW);
